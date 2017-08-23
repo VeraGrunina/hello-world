1519b4c89a08fec3b9bb86b6f9b5639231ed8e71
VeraGrunina <31181771+VeraGrunina@users.noreply.github.com>
2017-08-23 16:33:00 +0400

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package com.epam.homepack1;
+
+/**
+ * Created by Вера on 23.08.2017.
+ */
+public class Multiply {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a * b);
+    }
+}
---
+++ Plus.java
@@ -0,0 +1,14 @@
+package com.epam.homepack1;
+
+/**
+ * Created by Вера on 23.08.2017.
+ */
+public class Plus {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a + b);
+
+      //  System.out.println("Hello, world!");
+    }
+}
